92c327d05d74e68864476eb3ff0e24388b5defd2
rachel hazes <rahazes@gmail.com>
2015-02-03 12:20:07 -0500

    sessions with sqlite3, 3password.js

--- 3password.js
+++ 3password.js
@@ -1,34 +1,56 @@
 var express = require('express');
 var session = require("express-session");
 var bodyParser = require('body-parser');
-var secret = require('./secret.json');
 var sqlite3 = require('sqlite3').verbose();
-var db = sqlite3.Database("auth_exercise.db");
+var db = new sqlite3.Database("auth_exercise.db");
 
 var app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
-  secret: secret.password,
+  secret: "cookie",
   resave: false,
   saveUninitialized: true
 }));
-
-
 
 app.get('/', function (req, res) {
  res.sendFile(__dirname + '/public/index.html');
 });
 
+app.post('/user', function(req,res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var confirm = req.body.confirm;
+  if (password != confirm) {
+    res.redirect('/');
+  } else {
+    db.get("SELECT*FROM users WHERE username= ? AND password= ?", username, password, function(err,row) {
+      if(err) {throw err;}
+      if(row) {
+        res.sendFile(__dirname + '/public/exist.html');
+      } else {
+        db.run("INSERT INTO users (username,password) VALUES(?,?)", username, password, function(err) {
+          if(err){throw err;}
+          req.session.valid_user= true;
+          res.redirect('/secret_page');
+        });
+      }
+    });
+  }
+});
+
 app.post('/session', function (req, res) {
-  console.log("hey");
- if ( req.body.password === secret.password ) {
-   req.session.valid_user= true;
-   console.log(req.session);
-   res.redirect('/secret_page');
- }
- else {
-   res.redirect('/');
- }
+  var username = req.body.username;
+  var password = req.body.password;
+  db.get("SELECT*FROM users WHERE username= ? AND password= ?", username, password, function(err,row) {
+    if(err) {throw err;}
+    if(row) {
+      req.session.valid_user= true;
+      res.redirect('/secret_page');
+    } else {
+      res.sendFile(__dirname + '/public/error.html');
+    }
+  });
 });
 
 app.get('/secret_page', function (req, res) {
